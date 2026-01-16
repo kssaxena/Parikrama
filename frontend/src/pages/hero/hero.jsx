cfd061f120d210a907b5b70687fc8b1984d369cf
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-01-16 15:57:04 +0530

    feat: enhance responsive design in Hero component; adjust width and padding for better layout on different screen sizes

--- frontend/src/pages/hero/hero.jsx
+++ frontend/src/pages/hero/hero.jsx
@@ -28,7 +28,6 @@ const Hero = ({ stopLoading, startLoading }) => {
     try {
       startLoading();
       const response = await FetchData("admin/places", "get");
-      console.log(response);
       setData(response.data.data);
     } catch (err) {
       console.log(err);
@@ -51,21 +50,21 @@ const Hero = ({ stopLoading, startLoading }) => {
 
   return (
     <div className="flex justify-center items-center flex-col">
-      <div className="w-[90%]">
+      <div className="md:w-[90%] w-full">
         <RandomImageSlider
           images={galleryBannerImages}
-          className="lg:h-[300px] h-[200px]"
+          className="md:h-[300px] h-[200px]"
         />
         {/* <img
           src="https://ik.imagekit.io/pz8qfunss/Gallery/Gallery_Banner/create%20an%20image%20with%20a%20tag%20_Our%20new%20Collections_%20with%20adding%20jewelrries%20in%20the%20background%20write%20the%20text%20on%20the%20left%20or%20right%20side%20of%20the%20image%20and%20make%20it%20look%20asthetic.jpg?updatedAt=1751825753408"
           className="object-cover h-full w-full "
         /> */}
       </div>
-      <div className="flex  justify-center py-5 px-2">
-        <div className="w-96 h-96 bg-neutral-500 rounded-xl overflow-hidden">
+      <div className="flex  justify-center py-5 px-2 w-full">
+        <div className="w-96 h-96 bg-neutral-500 rounded-xl overflow-hidden lg:block hidden">
           <RandomImageSlider images={galleryBannerImages} />
         </div>
-        <div className="p-4">
+        <div className="md:p-4 w-fit overflow-hidden">
           <div className="flex justify-center items-center w-full">
             <InputBox
               Value={searchInput}
@@ -82,7 +81,7 @@ const Hero = ({ stopLoading, startLoading }) => {
             </button>
           </div>
           {data?.length > 0 ? (
-            <div className="flex gap-2 flex-col w-[650px]">
+            <div className="flex gap-2 flex-col xl:w-[650px]">
               {getRandomItems(data || [], 8).map((places) => (
                 <div key={places._id || places.id}>
                   <Card
@@ -103,7 +102,7 @@ const Hero = ({ stopLoading, startLoading }) => {
             <div>No data found...</div>
           )}
         </div>
-        <div className="w-96 h-96 bg-neutral-500 rounded-xl overflow-hidden">
+        <div className="w-96 h-96 bg-neutral-500 rounded-xl overflow-hidden lg:block hidden">
           <RandomImageSlider images={galleryBannerImages2} />
         </div>
       </div>
