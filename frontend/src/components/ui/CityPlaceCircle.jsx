e1851f45a3cf42c8b56d287bc12a469875cac9cd
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-02-05 23:29:59 +0530

    feat: refactor CityPlacesCircle component; add MobileMarker for improved marker functionality and tooltip display

--- frontend/src/components/ui/CityPlaceCircle.jsx
+++ frontend/src/components/ui/CityPlaceCircle.jsx
@@ -1,19 +1,97 @@
-import React from "react";
-import { FaMapMarkerAlt } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaHeart, FaMapMarkerAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { getDistanceKm } from "../../utils/DistanceCalculator";
 
-const CityPlacesCircle = ({ cityName, places = [] }) => {
-  const radius = 140; // distance from center
-  const centerSize = 120;
+const MobileMarker = ({ place, x, y, cityLat, cityLong }) => {
+  const [show, setShow] = useState(false);
+  const placeLat = place?.location?.coordinates?.[1];
+  const placeLong = place?.location?.coordinates?.[0];
+
+  const distance =
+    cityLat && cityLong && placeLat && placeLong
+      ? getDistanceKm(cityLat, cityLong, placeLat, placeLong).toFixed(1)
+      : null;
+
+  return (
+    <div
+      className="absolute group hover:z-50"
+      style={{ transform: `translate(${x}px, ${y}px)` }}
+    >
+      {/* DASHED LINE */}
+      <div
+        className="absolute top-1/2 right-1/2 origin-right border-t group-hover:border-none border-dashed border-gray-900 group-hover:z-0"
+        style={{
+          width: Math.sqrt(x * x + y * y),
+          transform: `rotate(${Math.atan2(y, x)}rad)`,
+        }}
+      />
+
+      {/* ICON */}
+      <Link
+        to={`/current/place/${place?._id}`}
+        onClick={(e) => {
+          if (!show) {
+            e.preventDefault();
+            setShow(true);
+            setTimeout(() => setShow(false), 2500);
+          }
+        }}
+        className="relative z-10 text-2xl cursor-pointer"
+      >
+        <FaMapMarkerAlt />
+      </Link>
+
+      {/* TOOLTIP */}
+      <div
+        className={`
+    absolute 
+    left-1/2 
+    -translate-x-1/2 
+    bottom-7
+    bg-black 
+    text-white 
+    px-3 
+    py-1 
+    rounded 
+    whitespace-nowrap
+    z-50
+    pointer-events-none
+    transition
+    ${show ? "opacity-100" : "opacity-0"}
+    group-hover:opacity-100
+  `}
+      >
+        <div className="w-24 h-24  bg-red-400">
+          <img
+            src={place?.images[0]?.url}
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="text-sm font-medium">{place.name}</div>
+        {distance && (
+          <div className="text-xs text-gray-300">{distance} km away</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const CityPlacesCircle = ({ cityName, places = [], cityLong, cityLat }) => {
+  const radius = 150; // distance from center
+  const centerSize = 130;
 
   return (
     <div className="relative md:w-[400px] w-full h-[400px] md:mx-auto flex items-center justify-center bg-gray-200 rounded-xl shadow">
       {/* CENTER CIRCLE */}
       <div
-        className="absolute flex items-center justify-center rounded-full bg-[#FFC20E] font-semibold text-lg shadow-lg"
+        className="absolute flex items-center justify-center rounded-full bg-[#FFC20E] font-semibold text-lg shadow-lg z-50 flex-col"
         style={{ width: centerSize, height: centerSize }}
       >
-        {cityName}
+        <span className="flex justify-center items-center gap-2">
+          I <FaHeart className="text-red-500" />
+        </span>
+        <span>{cityName}</span>
       </div>
 
       {/* PLACES AROUND CIRCLE */}
@@ -23,26 +101,14 @@ const CityPlacesCircle = ({ cityName, places = [] }) => {
         const y = radius * Math.sin(angle);
 
         return (
-          <div
+          <MobileMarker
             key={place._id || index}
-            className="absolute group"
-            style={{
-              transform: `translate(${x}px, ${y}px)`,
-            }}
-          >
-            {/* POINT */}
-            <Link
-              to={`/current/place/${place?._id}`}
-              className="w-4 h-4 rounded-full text-2xl cursor-pointer shadow-md"
-            >
-              <FaMapMarkerAlt />
-            </Link>
-
-            {/* TOOLTIP */}
-            <div className="absolute left-1/2 -translate-x-1/2 bottom-6 opacity-0 group-hover:opacity-100 transition bg-black text-white px-2 py-1 rounded whitespace-nowrap">
-              {place.name}
-            </div>
-          </div>
+            place={place}
+            x={x}
+            y={y}
+            cityLat={cityLat}
+            cityLong={cityLong}
+          />
         );
       })}
     </div>
