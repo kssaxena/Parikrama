28ca8a9f862a4a88210cb31c4961e3ae3a38650d
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-05-04 17:47:15 +0530

    feat changes -modification is done in model for adding maximum number of states in the country

--- backend/src/models/country.models.js
+++ backend/src/models/country.models.js
@@ -4,6 +4,7 @@ const countrySchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true, trim: true },
     code: { type: String, trim: true }, // IN,USA etc
+    totalStates: { type: String, trim: true },
   },
   { timestamps: true },
 );
